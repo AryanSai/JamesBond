73b04c63d7f291b6d12a65726e4fd430b11a2247
DMACS <aryansaiarvapelly@gmail.com>
2023-02-22 21:46:54 +0530

    text file based rules done

--- scripts/golden.py
+++ scripts/golden.py
@@ -1,6 +1,6 @@
 from brownie import accounts,GoldenContract
-import json,datetime,time,pytz
-
+import json,pytz
+from datetime import datetime, time
 nominalCurrencyList = ["AUD", "CAD", "EUR", "JPY", "NZD", "NOK", "GBP", "SEK", "CHF", "USD"]
 paymentConventionList = ["in arrears","in advance"]
 
@@ -9,7 +9,7 @@ with open("/home/dmacs/Desktop/JamesBond/IRS1.json", "r") as file:
 
 #take the list of rules and check on the individual rules taken from a text file
 
-def find_attribute_in_json(attribute):
+def find_attribute_in_json(data,attribute):
     results = []
     if isinstance(data, dict):
         for key, value in data.items():
@@ -28,20 +28,34 @@ def today():
     return int(current_date.timestamp())
 
 def getOperand(op):
-    if op == 'GBO' or op == 'FO Trade Capture' or op == 'Murex':
+    if op == 'GBO' or op == 'FOTradeCapture' or op == 'Murex':
         return op  
     elif op == 'SourceSystem':
         return data['header']['sourceSystem']
-    elif op == 'nominal':
-        find_attribute_in_json(op)
+    elif op == 'nominalCurrency':
+        return find_attribute_in_json(data,op)
+    elif op == 'paymentConvention':
+        return find_attribute_in_json(data,op) 
+    elif op == 'paymentConventionList':
+        return paymentConventionList       
+    elif op == 'fixingFrequency':
+        return find_attribute_in_json(data,op) 
+    elif op == 'paymentFrequency':
+        return find_attribute_in_json(data,op)         
+    elif op == 'settlementDate':
+        sd_list =  find_attribute_in_json(data,op)
+        result = []
+        for i in sd_list:
+            date = datetime.strptime(i, '%d-%m-%Y')
+            timestamp = int(date.timestamp())
+            result.append(timestamp)
+        return result    
     elif op == 'agreementDate':    
         return data['esperanto']['agreementDate']    
     elif op == 'nominalCurrencyList':
         return nominalCurrencyList
-    elif op == 'paymentConventionList':
-        return paymentConventionList   
     elif op == 'Today':
-        today()
+        return today()
 
 def readRules(goldenContract):
     with open('/home/dmacs/Desktop/JamesBond/rules.txt', 'r') as file:
@@ -50,17 +64,46 @@ def readRules(goldenContract):
             print('operand1 :', op1)
             print('operator :', op)
             print('operand2 :', op2)
-
             if op == '=':
                 print("Equality")
-                operand1 = getOperand(op1)
+                operand1=[]
+                op1 = getOperand(op1)
+                operand1.append(op1)
+                print(operand1)
                 operand2 = getOperand(op2)
-                goldenContract.isEqual(operand1,operand2, {"from": accounts[0]})
+                print(operand2)
+                if isinstance(operand1,list):
+                    for i in operand1:
+                        a=goldenContract.isEqual(i,operand2, {"from": accounts[0]})
+                        print(a)
+            elif op == '==':
+                print("Component wise Equality")
+                operand1 = getOperand(op1)
+                print(operand1)
+                operand2 = getOperand(op2)
+                print(operand2)
+                for i in range(len(operand1)):
+                    print(goldenContract.isEqual(operand1[i],operand2[i], {"from": accounts[0]}))
             elif op == '>':
                 print("Greater")
-                goldenContract.isGreater(operand1,operand2, {"from": accounts[0]})
+                operand1 = getOperand(op1)
+                print(operand1)
+                operand2 = getOperand(op2)
+                print(operand2)
+                if isinstance(operand1,list):
+                    for i in operand1:
+                        b=goldenContract.isGreater(i,operand2, {"from": accounts[0]})
+                        print(b)
             elif op == 'in':
                 print("inList")
+                operand1 = getOperand(op1)
+                print(operand1)
+                operand2 = getOperand(op2)
+                print(operand2)
+                if isinstance(operand1,list):
+                    for i in operand1:
+                        c = goldenContract.inList(i,operand2, {"from": accounts[0]})
+                        print(c)
             else:
                 print("Invalid Operand")
                 inputList=nominalCurrencyList
