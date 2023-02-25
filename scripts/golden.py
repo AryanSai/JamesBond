53ff0d93b63177a1fc340ac8671d03d597e47bd9
DMACS <aryansaiarvapelly@gmail.com>
2023-02-25 13:21:43 +0530

    rules from text generalised

--- scripts/golden.py
+++ scripts/golden.py
@@ -8,6 +8,35 @@ paymentConventionList = ["in arrears","in advance"]
 with open("/home/dmacs/Desktop/JamesBond/IRS1.json", "r") as file:
         data = json.load(file)
 
+
+def getSection(sourceSytem):
+    with open('/home/dmacs/Desktop/JamesBond/rules.txt', 'r') as file:
+        file_content = file.read()
+    sections = file_content.split('\n\n')
+
+    for section in sections:
+        lines = section.split('\n')
+        if len(lines) > 0:
+            first_line = lines[0]
+            if first_line.startswith('key:header-sourceSystem = value:GBO'):
+                print('Found GBO')
+                gbo =  section
+            elif first_line.startswith('key:header-sourceSystem = value:FO Trade Capture'):
+                print('Found FO')
+                fo = section
+            elif first_line.startswith('key:header-sourceSystem = value:Murex'):
+                print('Found Murex')
+                murex =  section    
+            else:
+                print('Unknown section:')
+
+    if sourceSytem == 'GBO':
+        return gbo
+    elif sourceSytem == 'FO Trade Capture':
+        return fo
+    elif sourceSytem == 'Murex':
+        return murex                            
+
 def get_timestamp(dt):
     date = datetime.strptime(dt, '%d-%m-%Y')
     timestamp = int(date.timestamp())
@@ -43,47 +72,48 @@ def today():
     return stamp
 
 def readRules(goldenContract):
-    with open('/home/dmacs/Desktop/JamesBond/rules.txt', 'r') as file:
-        for rule in file:
-            words = rule.split()
-            op1 = words[0]
-            op = words[1]
-            op2 = ' '.join(words[2:])  #for special case of FO Trade Capture
-            print('operand1 :', op1)
-            print('operator :', op)
-            print('operand2 :', op2)
+    
+    section  =getSection('GBO')
 
-            if op == '=':
-                print("Equality")
-                operand1=fetch_operand(op1)
-                print('operand 1:', operand1)
-                operand2=fetch_operand(op2)
-                print('operand 2:', operand2)
-                print(goldenContract.isEqual(operand1, operand2, {"from": accounts[0]}))
-            
-            elif op == '>':
-                #should be integer
-                print("Greater")
-                operand1=fetch_operand(op1)
-                print('operand 1:', operand1)
-                operand2=fetch_operand(op2)
-                print('operand 2:', operand2)
-                print(goldenContract.isGreater(operand1, operand2, {"from": accounts[0]}))
-
-            elif op == 'in':
-                print("inList")
-                operand1 = fetch_operand(op1)
-                print(operand1)
-                #should generalise this
-                if op2 == 'nominalCurrencyList':
-                    operand2 = nominalCurrencyList
-                elif op2 == 'paymentConventionList':
-                    operand2 = paymentConventionList  
-                print(operand2)
-                print(goldenContract.inList(operand1,operand2, {"from": accounts[0]}))   
-                 
-            else:
-                print("Invalid Operand")
+    section = section.split('\n')
+    for rule in section:
+        words = rule.split()
+        op1 = words[0]
+        op = words[1]
+        op2 = ' '.join(words[2:])  #for special case of FO Trade Capture
+        print('operand1 :', op1)
+        print('operator :', op)
+        print('operand2 :', op2)
+        if op == '=':
+            print("Equality")
+            operand1=fetch_operand(op1)
+            print('operand 1:', operand1)
+            operand2=fetch_operand(op2)
+            print('operand 2:', operand2)
+            print(goldenContract.isEqual(operand1, operand2, {"from": accounts[0]}))
+        
+        elif op == '>':
+            #should be integer
+            print("Greater")
+            operand1=fetch_operand(op1)
+            print('operand 1:', operand1)
+            operand2=fetch_operand(op2)
+            print('operand 2:', operand2)
+            print(goldenContract.isGreater(operand1, operand2, {"from": accounts[0]}))
+        elif op == 'in':
+            print("inList")
+            operand1 = fetch_operand(op1)
+            print(operand1)
+            #should generalise this
+            if op2 == 'nominalCurrencyList':
+                operand2 = nominalCurrencyList
+            elif op2 == 'paymentConventionList':
+                operand2 = paymentConventionList  
+            print(operand2)
+            print(goldenContract.inList(operand1,operand2, {"from": accounts[0]}))   
+             
+        else:
+            print("Invalid Operand")
 def main():
     goldenContract = GoldenContract.deploy({"from":accounts[0]})
     readRules(goldenContract)
